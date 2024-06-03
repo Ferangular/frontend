df8de650067cd2b9d5670cdd63b04401125c61e7
Ferangular <firthunsnpm@gmail.com>
2024-06-03 22:10:04 +0200

    Remove ignored directories from version control

--- src/app/core/constants/constans.ts
+++
@@ -1,4 +0,0 @@
-import {InjectionToken} from "@angular/core";
-
-export const JSON_FILE = 'assets/app-settings/app-settings.json';
-export const APP_SETTINGS_PATH = new InjectionToken('APP_SETTINGS_PATH');
