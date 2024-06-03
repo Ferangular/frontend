b83a663a30d4ab42c86ec0a55b976fde7bef4a41
Ferangular <firthunsnpm@gmail.com>
2024-06-03 12:22:36 +0200

    feat: add provide in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers:
   [
-    provideRouter(routes, withViewTransitions())
-
+    provideRouter(routes, withViewTransitions()),
+    provideHttpClient()
   ]
 };
